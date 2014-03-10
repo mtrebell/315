73833640829f15c0be5ea3f744082e9cc5b02052
robert Babiak <babiakr3@mymail.macewan.ca>
2014-03-10 10:26:12 -0600

    moving the css and js code around to clean up the code.

--- Scripts/MainPage.js
+++ Scripts/MainPage.js
@@ -233,4 +233,151 @@ function MovieOlderThanDays(currentD, movieD, daysOlderThan)
     return false;
 
 }
+//------------------------------------------------------------------------
+// Grid View Code
+//------------------------------------------------------------------------
+
+function GenerateMovieGrid(srcData, dest, moviesPerRow)
+/*
+    GenerateMovieGrid:
+        Creates a table and displays all covers within
+        the current filter.
+
+        srcData:
+            source data containing divs of class cover or cover-not-loaded
+
+        dest:
+            destination div to stuff the generated table into
+
+        moviesPerRow:
+            the number of movies to display per row
+*/
+{
+
+    $(dest).css('display', 'table');
+    var i = 0, divObj;
+    $(srcData + ' .cover').each(function () {
+        //If movie is not in the current filter, skip it
+        if (!CoverFilter($(this))) return;
+
+        var curRow = parseInt(i / moviesPerRow);
+
+        //create new row if necessary
+        if (i % moviesPerRow == 0) {
+            divObj = document.createElement('div');
+            $(divObj).addClass('gridRow' + curRow).css('display', 'table-row').appendTo($(dest));
+        }
+
+        var movie = $(this).clone();
+        var movieInfo = $(movie).find('#info');
+
+        //if movie cover isn't loaded, construct image data
+        if ($(movie).hasClass("cover-not-loaded")) {
+            var newMovie = document.createElement('img');
+            $(newMovie).attr('id', $(movie).attr('id')).attr('data-src', $(movie).attr('dataUrl'));
+            $(newMovie).removeAttr('dataUrl').attr('cfIndex', i);
+            movie = $(newMovie);
+        } else {
+            //otherwise just grab the image data
+            movie = $(movie).find('img');
+            var tempUrl = $(movie).attr('src');
+            $(movie).removeAttr('src').attr('data-src', tempUrl).attr('cfIndex', i);
+        }
+
+        var dispData = '<h1>' + $(movie).attr('id') + '</h1><div></div>';
+        $(movie).removeAttr('style').attr('height', '300px').attr('width', '200px').show()
+
+        var container = document.createElement('div');
+        $(container).css({
+            'display': 'table-cell',
+            'padding': '5px'
+        }).addClass('gridCover');
+
+        $(container).append(movie, movieInfo);
+        $(container).appendTo(dest + ' .gridRow' + curRow);
+        i++;
+    });
+}
+
+function MovieGridShowInView()
+/*
+MoviesGridShowInView
+    Loads the images of movies in the grid view based on
+    whether they are "in view" or not.
+
+    Also, while it loops each image, it adds a click
+    method to take the user to that movie on the coverflow
+*/
+{
+    $("#GridDialog").find($(".gridCover")).each(function(cover){
+        var img = $(this).find('img');
+        if ($(this).visible(true)) {
+            //if image is visible
+            if (!$(img).attr('src')){
+                //set the img src attribute
+                $(img).attr('src', $(img).attr('data-src')).click(function(e){
+                    //when image is clicked, exit grid view and set main page
+                    //to clicked image.
+                    $('#GridDialog').dialog('close');
+
+
+                    //if the movie is within 10 covers of the coverflows current index
+                    //then animate, otherwise, jump
+                    var curIndex = coverFlowCtrl.coverflow('index');
+                    var cfIndex = $(img).attr('cfIndex');
+                    //coverflow shows 9 movies on both sides of the current one
+                    if (cfIndex >= curIndex - 9 && cfIndex <= curIndex + 9) {
+                        coverFlowCtrl.coverflow('index', cfIndex);
+                    } else {
+                        coverFlowCtrl.coverflow('index', cfIndex, false);
+                    }
+                });
+            }
+        }
+    });
+}
+
+
+//------------------------------------------------------------------------
+// Movie Review Code
+//------------------------------------------------------------------------
+function GetMovieReviewIMDB(ui)
+// this will get the IMDB reviews into the new tab
+{
+    console.log("IMDB: %o", ui.newPanel.attr("dataUrl"));
+    $.ajax({
+        type: "POST",
+        url: "MainPage.aspx/GetIMDbReviews",
+        data: JSON.stringify({ 'imdbID': ui.newPanel.attr("dataUrl") }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: true,
+        success: function (msg) {
+            var div = msg.d;
+            console.log(msg);
+            ui.newPanel.html(div);
+        },
+        cache: false
+    });
+}
+
+function GetMovieReviewRotten(ui)
+// This will get the rotten tomato reviews into the selected tab.
+{
+    $.ajax({
+        type: "POST",
+        url: "MainPage.aspx/GetRottenReviews",
+        data: JSON.stringify({ 'imdbID': ui.newPanel.attr("dataUrl") }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        async: true,
+        success: function (msg) {
+            var div = msg.d;
+            console.log(msg);
+            ui.newPanel.html(div);
+        },
+        cache: false
+    });
+}
+
 
