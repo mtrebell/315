b2732c3ce74ae555c77dd055d44ed0009ffaa53e
Melissa Trebell <mtrebell@gmail.com>
2014-03-10 08:26:40 -0600

    Add code logic for fetching video

--- FetchVideo.aspx.cs
+++ FetchVideo.aspx.cs
@@ -1,5 +1,5 @@
-﻿//Baed off of http://www.robgreen.me/post/Getting-Started-With-The-YouTube-API-in-C.aspx
-
+﻿//Based off of http://www.robgreen.me/post/Getting-Started-With-The-YouTube-API-in-C.aspx
+//FOR ADD TO DATA BASE
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -46,3 +46,45 @@ public partial class _Default : System.Web.UI.Page
     }
 }
 
+/*** FOR MAIN.ASPX.CS
+*[WebMethod()]
+*string getURL(string mov_id){
+ * SqlDataReader reader = null; // return object
+        SqlConnection conn = new SqlConnection(Middleware.ConnectionString); // create database connection
+        conn.Open();
+        using (SqlCommand comm = new SqlCommand())      // create query
+        {
+            comm.Connection = conn;
+            comm.CommandType = System.Data.CommandType.Text; // indicate query as procedure
+            comm.CommandText = string.Format("Select [mov_id] From [MovieSummary] Where [mov_id] = '{0}'", mov_id);
+            reader = comm.ExecuteReader(System.Data.CommandBehavior.CloseConnection); // execute query
+
+            if (reader.HasRows)
+            {
+                reader.Read();
+                return reader[0].ToString();
+             }
+        }
+ }
+
+//--------for main.aspx---------//
+ 
+button = Traler id = mov_id onclick = getTrailer(this.id)
+ 
+getTrailer(movieId)
+$.ajax({
+		type: "GET",
+		url: main.aspx/getUrl
+		data: movieId
+		dataType:"json",
+success: function(response){
+	console.log(response);			
+if(response.data.items){
+var movie = response.data.items[0];
+var id=movie.id;
+//Add video	
+var frame="<iframe  type='text/html' width='425' height='349' src=' http://www.youtube.com/embed/" +id+ "' frameborder='0'></iframe>";
+
+$("#trailer").html(frame);		
+}
+*/
